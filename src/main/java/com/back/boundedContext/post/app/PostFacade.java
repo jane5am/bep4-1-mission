f28954c247c2d2fa88ff42da45c212a3cd9a60cb
hyejin <hyejda00@naver.com>
2026-09-18 09:45:14 +0900

    11. RsData 도입 - Post 모듈 내 도입

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -3,6 +3,7 @@ package com.back.boundedContext.post.app;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +25,7 @@ public class PostFacade {
     return postRepository.findById(id);
   }
 
-  public Post write(Member author, String title, String content){
+  public RsData<Post> write(Member author, String title, String content){
     return postWriteUseCase.write(author, title, content);
   }
 
---
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -0,0 +1,16 @@
+package com.back.global.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData<T> {
+  private final String resultCode; // 200-1 (기본성공), 200-2(보내기 성공, 파일 첨부성공 등)
+  private final String msg;
+  private final T data;
+
+  public RsData(String resultCode, String msg) { // 이건 data 가 없는 경우 
+    this(resultCode, msg, null);
+  }
+}
