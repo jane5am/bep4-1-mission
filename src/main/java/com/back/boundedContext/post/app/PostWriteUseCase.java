147bb577fdc804fa7d054bc7c228480ed4615423
hyejin <hyejda00@naver.com>
2026-09-18 10:51:02 +0900

    14. 랜덤 팁 메서드 호출에서 HTTP API 호출로 변경

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -6,6 +6,7 @@ import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
+import com.back.shared.member.out.MemberApiClient;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +18,13 @@ public class PostWriteUseCase {
 
   private final PostRepository postRepository;
   private final EventPublisher eventPublisher;
-  private final MemberFacade memberFacade; // 모듈간의 결합도가 생김!! 이러면 안됨
+//  private final MemberFacade memberFacade; // 모듈간의 결합도가 생김!! 이러면 안됨
+  private final MemberApiClient memberApiClient;
 
   public RsData<Post> write(Member author, String title, String content) {
     Post post = postRepository.save(new Post(author, title, content)); // 저장먼저!
     eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
-    String randomSecureTip = memberFacade.getRandomSecureTip();
+    String randomSecureTip = memberApiClient.getRandomSecureTip();
 
     return new RsData<>(
             "201-1",
