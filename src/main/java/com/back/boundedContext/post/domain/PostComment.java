4bad75f3df139569cfe92be3dcc475d8cde03c78
hyejin <hyejda00@naver.com>
2026-09-18 11:31:15 +0900

    15. DB 결합도 줄이기 위해 분리 및 POST_MEMBER 테이블 생성

--- src/main/java/com/back/boundedContext/post/domain/PostComment.java
+++ src/main/java/com/back/boundedContext/post/domain/PostComment.java
@@ -5,6 +5,7 @@ import com.back.global.jpa.entity.BaseIdAndTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import static jakarta.persistence.FetchType.LAZY;
 @Getter
 @Entity
 @NoArgsConstructor
+@Table(name="POST_POST_COMMENT")
 public class PostComment extends BaseIdAndTime {
     @ManyToOne(fetch = LAZY)
     private Post post;
